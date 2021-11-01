d4a75545f73f04b4c82d080c74968518efd08587
Andrey <kochetpwr@gmail.com>
2021-11-01 13:58:50 +0400

    commit

---
+++ 2 Semestr/OP/LR_5/LR_5/DOP_05.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR_5
+{
+    class DOP_05 : IComparer<IVector>
+    {
+        public int Compare(IVector a1, IVector a2)
+        {
+            return Math.Sign(a1.GetNorm() - a2.GetNorm());
+        }
+    }
+}
---
+++ 2 Semestr/OP/LR_5/LR_5/Vectors.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR_5
+{
+    class Vectors
+    {
+        public static IVector Sum(IVector a1, IVector a2)
+        {
+            if (a1.Lenght == a2.Lenght)
+            {
+                Array_vector a3 = new Array_vector(a1.Lenght);
+                for (int i = 0; i < a1.Lenght; i++)
+                {
+                    a3[i] = a2[i] + a1[i];
+
+                }
+                return a3;
+            }
+            else
+            {
+                Console.WriteLine("действие невозможно выполнить из-за разности длин");
+                Array_vector a3 = new Array_vector(1);
+                return a3;
+            }
+        }
+        public static double Scalar(IVector a1, IVector a2)
+        {
+            if (a1.Lenght == a2.Lenght)
+            {
+                double sum = 0;
+                for (int i = 0; i < a1.Lenght; i++)
+                {
+                    sum += a1[i] * a2[i];
+                }
+                double sum2 = 0;
+                double sum3 = 0;
+                for (int i = 0; i < a1.Lenght; i++)
+                {
+                    sum2 = sum2 + a1[i] * a1[i];
+                    sum3 = sum3 + a1[i] * a1[i];
+                }
+                return (Math.Abs(sum) / (Math.Sqrt(sum2) * Math.Sqrt(sum3)));
+            }
+            else
+            {
+                Console.WriteLine("действие невозможно выполнить");
+                return 0;
+            }
+
+        }
+        public static double GetNorm(IVector a1)
+        {
+            double sum = 0;
+            for (int i = 0; i < a1.Lenght; i++)
+            {
+                sum = sum + a1[i] * a1[i];
+            }
+            return Math.Sqrt(sum);
+        }
+        public static IVector Fmin(IVector[] mas)
+        {
+            IVector min = mas[0];
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[0].GetNorm().CompareTo(mas[1].GetNorm());
+            }
+            return min;
+        }
+    }
+}
